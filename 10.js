7ae050a51e99d7fc5662394deabbc06faed6d7e2
Glenn Gailey <ggailey777@hotmail.com>
2019-10-28 11:18:38 -0700

    updated to handle muliple rules

--- 10.js
+++ 10.js
@@ -8,58 +8,124 @@ const ENCODING = 'utf8';
 
 console.log('\nRunning tensies...\n');
 
+// Output for --rules.
 if (args.rules) {
   const keys = Object.keys(rules);
+  console.log('Available rules:');
   keys.forEach(key => {
-    console.log('Available rules:');
     console.log(` - ${key}: ${rules[key].description}`);
   });
   return;
-} 
+}
+
+const sourceFileNames = [];
+//const processedFileNames = [];
+const processedFiles = [];
+const rulesToRun = [];
+
+// Get the rules we need to run.
+if (args.all) {
+  for (var ruleName in rules) {
+    rulesToRun.push(rules[ruleName]);
+  } 
+}else {
+  args._.forEach(ruleName => {
+    rulesToRun.push(rules[ruleName]);
+  });
+}
 
 try {
 
-  const ruleName = args._[0];
-  const rule = rules[ruleName];
-
-  const fileNames = [];
-  const processedFileNames = [];
-
-  if (rule) {
-    console.log(`[RULE] ${ruleName}: ${rule.description}`);
-
-    fs.readdirSync(config.filePath).forEach(fileName => {
-      if(/\.md/.test(fileName)) {
-        fileNames.push(fileName);
-      }
-    });
-
-    fileNames.forEach(fileName => {
-      const shouldProcessFiles = processedFileNames.length < config.threshold;
-
-      if (shouldProcessFiles) {
-        const filePath = path.join(config.filePath, fileName);
-        let text = fs.readFileSync(filePath, ENCODING);
-        const isMatch = rule.pattern.test(text);
-
-        if (isMatch) {
-          processedFileNames.push(fileName);
-          text = rule.run(text);
-          fs.writeFileSync(filePath, text, ENCODING);
-        }
-      }
-    });
-
-    console.log(`\nNumber of files modified: ${processedFileNames.length}`);
-
-    if (processedFileNames.length > 0) {
-      console.log("\n - " + processedFileNames.join("\n - "));
-    } else {
-      console.log("\nNo files were modified by this rule.");
+  // Load the list of files in the directory.
+  fs.readdirSync(config.filePath).forEach(fileName => {
+    if (/\.md/.test(fileName)) {
+      sourceFileNames.push(fileName);
     }
+  });
 
-    console.log("\nDone");
-  }
-} catch(e) {
+  // Process each requested rule.
+  rulesToRun.forEach(rule => {
+    //for(let i = 0; i < args._.length; i++) {
+
+    // //const ruleName = args._[i];
+    // // Get the rule with the current name.
+    // const rule = rules[ruleName];
+
+    // if (rule) {
+      //@craigshoemaker not sure how to the get the key of the current 'rule'.
+      console.log(`[RULE] ${rule.description}`);
+
+      // If we've already processed some files, check them first.
+      if (processedFiles.length > 0) {
+        processedFiles.forEach(file => {
+          processMatchingFiles(file, rule);
+        });
+      }
+
+      let j = 0;
+      // If we don't have all X files, add some in this pass.
+      while (processedFiles.length < config.threshold) {
+        const isProcessed = false;
+
+        // Have we checked a given file for the current rule.
+        processedFiles.forEach(p => {
+          if (p.name === sourceFileNames[j]) {
+            isProcessed = true;
+          }
+        });
+
+        // Process a new file.
+        if (!isProcessed) {
+          const file = new Object;
+          file.name = sourceFileNames[j];
+
+          file.path = path.join(config.filePath, file.name);
+          file.text = fs.readFileSync(file.path, ENCODING);
+
+          // Add if the file matches the rule and isn't in the array.
+          if (processMatchingFiles(file, rule)) {
+            if (!processedFiles.includes(file)) {
+              processedFiles.push(file);
+            }
+          }
+        }
+        j++;
+      //}
+    };
+  });
+
+  // Write out processed files.
+  processedFiles.forEach(file => {
+    fs.writeFileSync(file.path, file.text, ENCODING);
+    console.log("- " + file.name);
+  });
+
+
+  // @craigshoemaker I'm not sure how to best do this. 
+  // Probably we need to build a logging object that gets loaded with each run.
+  //console.log(`\nNumber of files modified: ${processedFiles.length}`);
+
+  // if (processedFiles.length > 0) {
+  //   processedFiles.forEach(file => {
+  //     console.log("\n - " + file.name.join("\n - "));
+  //   });
+
+  // } else {
+  //   console.log("\nNo files were modified by this rule.");
+  // }
+
+  // console.log("\nDone");
+}
+catch (e) {
   console.log(`Error: ${JSON.stringify(e)}`);
+};
+
+function processMatchingFiles(file, rule) {
+  // Does the file text match the rule. 
+  const isMatch = rule.pattern.test(file.text);
+  // Run the rule if a match.
+  if (isMatch) {
+    file.text = rule.run(file.text);
+  }
+  return isMatch;
 }
--- rules.js
+++ rules.js
@@ -1,11 +1,38 @@
 module.exports = {
 
   trimTitle: {
-    description: "Trim \"|\" and anything after it from the metadata title",
+    description: "Trim \"|\" and anything after it from the metadata title.",
     pattern: /(title:.*)(\|.*)/,
     run: function(content) {
       return content.replace(this.pattern, "$1");
     }
   },
-
+  replaceManager: {
+    description: "Replace \"jeconnoc\" with manager \"gwallace\".",
+    pattern: /(manager:.*)(jeconnoc.*)/,
+    run: function(content) {
+      return content.replace(this.pattern, "$1gwallace");
+    }
+  },
+  removeServices: {
+    description: "Remove the unused \"services:\" metadata.",
+    pattern: /services:.*\m/,
+    run: function(content) {
+      return content.replace(this.pattern, "");
+    }
+  },
+  removeDocumentationCenter: {
+    description: "Remove the unused \"documentationcenter:\" metadata.",
+    pattern: /documentationcenter:.*\m/,
+    run: function(content) {
+      return content.replace(this.pattern, "");
+    }
+  },
+  resetFreshness: {
+    description: "Update the freshness for all processed articles.",
+    pattern: /ms.date:.*/,
+    run: function(content) {
+      return content.replace(this.pattern, "$1" + "10/28/2019");
+    }
+  },
 };
