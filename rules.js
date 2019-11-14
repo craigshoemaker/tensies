b49b4e3d4f31cc146507d3103e439fda3af0ae82
= <craig@craigshoemaker.net>
2019-11-14 10:23:47 -0500

    Convert pattern to getPattern function

--- rules.js
+++ rules.js
@@ -1,11 +1,9 @@
 module.exports = {
-
   trimTitle: {
-    description: "Trim \"|\" and anything after it from the metadata title",
-    pattern: /(title:.*)(\|.*)/,
+    description: 'Trim "|" and anything after it from the metadata title',
+    getPattern: () => /(title:.*)(\|.*)/,
     run: function(content) {
-      return content.replace(this.pattern, "$1");
+      return content.replace(this.getPattern(), "$1");
     }
   },
-
 };
